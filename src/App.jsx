fec8a1f40035b6ec00621cfb4f74d2f161e274ec
nari-0v0 <ej9811@naver.com>
2025-07-18 14:23:15 +0900

    과제 진행

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,60 @@
-import React, { useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.css';
+
 
 function App() {
   const [query, setQuery] = useState('');
 
-  const handleChange = (event) => {
-    setQuery(event.target.value);
-    console.log('검색 쿼리:', event.target.value);
+  const debounce = (func, delay) => {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer)
+      timer = setTimeout(() => func(...args), delay)
+    };
   };
+
+  const debouncedSearch = useMemo(
+    () =>
+      debounce((query) => {
+        console.log('디바운스 검색 쿼리 :', query)
+      }, 300),
+    []
+  );
+
+  const handleChangeDebounced = useCallback(
+    (event) => {
+      setQuery(event.target.value);
+      debouncedSearch(event.target.value);
+    },
+    [debouncedSearch]
+  );
+
+  const throttle = (func, delay) => {
+    let lastTime = 0;
+    return (...args) => {
+      const now = Data.now();
+      if (now - lastTime >= delay) {
+        func(...args);
+        lastTime = now;
+      }
+    };
+  };
+
+  const throttledSearch = useMemo(
+    () =>
+      throttle((query) => {
+        console.log('스로틀 검색 쿼리 :', query);
+      }, 300),
+    []
+  );
+
+  const handleChangeThrottled = useCallback(
+    (event) => {
+      setQuery(event.target.value);
+      throttledSearch(event.target.value);
+    },
+    [throttledSearch]
+  );
 
   return (
     <div className="container">
@@ -21,7 +68,7 @@ function App() {
         <input
           type="text"
           placeholder="Debounce를 이용한 검색..."
-          onChange={handleChange}
+          onChange={handleChangeDebounced}
         />
       </div>
       <div>
@@ -29,11 +76,11 @@ function App() {
         <input
           type="text"
           placeholder="Throttle을 이용한 검색..."
-          onChange={handleChange}
+          onChange={handleChangeThrottled}
         />
       </div>
     </div>
   );
-}
+};
 
 export default App;
